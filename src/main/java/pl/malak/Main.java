b15ab7dae319814b0a7d0f235dcd069a88213692
michal.szpruta <michal.szpruta@bitbar.com>
2015-07-01 23:13:13 +0200

    Test data included

--- src/main/java/pl/malak/Main.java
+++ src/main/java/pl/malak/Main.java
@@ -6,7 +6,7 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        File file = new File("C:\\Users\\maszter\\Downloads\\WZÓR.xlsx");
+        File file = new File("WZÓR.xlsm");
         Report report = new Report();
         report.create(file);
         report.printEmployersWithEmployees();

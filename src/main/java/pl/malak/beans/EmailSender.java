e1633178691670c69b2295dac66cc82b88320b82
michal.szpruta <michal.szpruta@bitbar.com>
2015-11-03 19:36:35 +0100

    Email config przepisany

--- src/main/java/pl/malak/beans/EmailSender.java
+++ src/main/java/pl/malak/beans/EmailSender.java
@@ -1,52 +1,67 @@
 package pl.malak.beans;
 
-import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
-import javax.annotation.Resource;
 import javax.mail.BodyPart;
 import javax.mail.Message.RecipientType;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeBodyPart;
 import javax.mail.internet.MimeMessage;
 import javax.mail.internet.MimeMultipart;
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.Properties;
-
 
 /**
  * @author Michał Szpruta - szprutamich@gmail.com
  */
 @Service
-public class EmailSender {
+public class EmailSender extends JavaMailSenderImpl {
 
     private static final String DEFAULT_ENCODING = "UTF-8";
 
     private static final String DEFAULT_MIME = "text/html; charset=UTF-8";
 
+    @Value("${mail.sender.from}")
     private String emailFrom;
 
-    @Resource
-    private JavaMailSender mailSender;
+    @Value("${mail.sender.username}")
+    private String userName;
+
+    @Value("${mail.sender.pass}")
+    private String pass;
+
+    @Value("${mail.sender.host}")
+    private String host;
+
+    @Value("${mail.sender.port}")
+    private Integer port;
+
+    @Value("${mail.sender.protocol}")
+    private String protocol;
+
+    @Value("${mail.sender.auth}")
+    private String auth;
+
+    @Value("${mail.sender.starttls}")
+    private Boolean starttls;
 
     @PostConstruct
-    private void setEmailFrom() {
-        InputStream is = this.getClass().getClassLoader().getResourceAsStream("malak.properties");
-        Properties properties = new Properties();
-        try {
-            properties.load(is);
-            emailFrom = properties.getProperty("mail.sender.from");
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+    private void setProperties() {
+        setUsername(userName);
+        setPassword(pass);
+        setHost(host);
+        setPort(port);
+        setProtocol(protocol);
+        getJavaMailProperties().put("mail.debug", false);
+        getJavaMailProperties().put("mail.smtp.starttls.enable", starttls);
+        getJavaMailProperties().put("mail.smtp.auth", auth);
     }
 
     public boolean send(String subject, String body, String email, BodyPart... attachments) {
         if (emailFrom != null) {
             try {
-                MimeMessage msg = mailSender.createMimeMessage();
+                MimeMessage msg = createMimeMessage();
                 msg.setSubject(subject, DEFAULT_ENCODING);
                 msg.addRecipient(RecipientType.TO, new InternetAddress(email));
                 msg.setFrom(new InternetAddress(emailFrom));
@@ -58,7 +73,7 @@ public class EmailSender {
                     multiPart.addBodyPart(att);
                 }
                 msg.setContent(multiPart);
-                mailSender.send(msg);
+                send(msg);
             } catch (Exception ex) {
                 return false;
             }
